99964fa2f3587f128885f603316bb10668334246
jslee <etoileettoi@gmail.com>
2020-07-05 10:24:02 +0900

    입력한 내용을 반영해주기

--- app/src/main/java/kr/co/tjoeun/kotlinbasic_20200704/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/kotlinbasic_20200704/MainActivity.kt
@@ -11,6 +11,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+//        적용하기 버틴이 눌린 경우
+        applyBtn.setOnClickListener {
+//            inputEdt에 입력된 내용(text속성의 값)을 받아와서 (get)
+            val inputContent = inputEdt.text.toString()
+
+//            resultTxt의 내용(text속성의 값)으로 반영 (set)
+            resultTxt.text = inputContent
+        }
+
         touchBtn.setOnClickListener {
             //터치 버튼이 눌리면 실행될 코드를 적는 공간
 
@@ -20,8 +29,6 @@ class MainActivity : AppCompatActivity() {
             Log.d("메인화면", "버튼 눌림")
 
             Log.e("메인화면", "에러시 로그 찍을 때 사용")
-
-
 
         }
     }

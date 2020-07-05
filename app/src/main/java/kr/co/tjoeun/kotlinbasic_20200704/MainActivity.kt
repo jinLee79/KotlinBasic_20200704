d92ddaa656787172a57164b1bde995c2d0b3b374
jslee <etoileettoi@gmail.com>
2020-07-05 11:06:42 +0900

    코틀린 기초 문법 연습

--- app/src/main/java/kr/co/tjoeun/kotlinbasic_20200704/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/kotlinbasic_20200704/MainActivity.kt
@@ -21,14 +21,31 @@ class MainActivity : AppCompatActivity() {
         }
 
         touchBtn.setOnClickListener {
-            //터치 버튼이 눌리면 실행될 코드를 적는 공간
+//            터치 버튼이 눌리면 실행될 코드를 적는 공간
 
-            //Toast는 JAVA시절부터 쓰던 클래스. static method
+//            Toast는 JAVA시절부터 쓰던 클래스. static method
             Toast.makeText(this, "버튼 눌림", Toast.LENGTH_SHORT).show()
 
             Log.d("메인화면", "버튼 눌림")
 
             Log.e("메인화면", "에러시 로그 찍을 때 사용")
+
+//            Kotlin 문법 / 개념 확인
+            val num1 = 10
+            num1.toString()  // Int도 참조형 변수임을 확인
+
+//            when 활용 연습
+            when (num1) {
+                10 -> {
+                    Log.d("조건문연습", "num1에 10이 있다")
+                    Log.d("조건문연습", "num1에 10이 있을 때 둘째줄")
+                }
+                15 -> {Log.d("조건문연습","num1에 15이 있다")}
+                17, 18, 19 -> {Log.d("조건문연습","num1이 17, 18, 19 중 하나다")}
+                in 20..29 -> {Log.d("조건문연습","num1에 20~29다")}
+                else -> {Log.d("조건문연습" , "그외의 숫자")}
+            }
+
 
         }
     }
